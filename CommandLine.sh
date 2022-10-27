9b80123eb8409f95613ee23591fb3a943ced29d6
Mamiglia <33762284+Mamiglia@users.noreply.github.com>
2022-10-27 19:56:04 +0200

    Fix command line not considering missing users
     'id = -1' -> 'User was not Found'

--- CommandLine.sh
+++ CommandLine.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-ids=$(cat instagram_posts.csv| cut --fields=2,8 -s | grep -oE '^[[:digit:]]+[[:blank:]]+.{100,}$' | head -10 | cut -f1 | mawk 8 ORS="\|")
+ids=$(cat instagram_posts.csv| cut --fields=2,8 -s | grep -oE '^-?[[:digit:]]+[[:blank:]]+.{100,}$' | head -10 | cut -f1)
+
+while IFS= read -r id; do
+	if [ "$id" = -1 ]; then
+		echo User was not Found
+	else
+		cat instagram_profiles.csv | cut -f 1,3 | grep -w "$id" | cat
+	fi
+done <<< "$ids"
 
 
-cat instagram_profiles.csv | cut -f 1,3 | grep -w "${ids::-2}" | cat
